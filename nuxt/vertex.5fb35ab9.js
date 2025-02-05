e92965907d2f10ffc4dfd29e79a47cbd3ef533e3
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-02-05 17:20:28 +0100

    Fixed MangaDB Image Previews

---
+++ nuxt/vertex.5fb35ab9.js
@@ -0,0 +1,1 @@
+import"./entry.3ed8a892.js";const i=""+globalThis.__publicAssetsURL("img/mangadb.png"),_=""+globalThis.__publicAssetsURL("img/picross.png"),o=""+globalThis.__publicAssetsURL("img/lego.png"),p=""+globalThis.__publicAssetsURL("img/vertex.png");export{i as _,_ as a,o as b,p as c};
