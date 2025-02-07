c697c0763eaf0af49c482917dd2d9864c3bfdd25
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-02-07 16:09:33 +0100

    Questions fixed (this time fr)

--- projects/verschubleiter/nuxt/config.51c76e7b.js
+++
@@ -1,1 +0,0 @@
-import{cj as p,aV as o,l as i}from"./entry.73be0ea7.js";const t={nuxt:{}},a=p(t);function s(){const n=i();return n._appConfig||(n._appConfig=o(a)),n._appConfig}export{s as u};
--- projects/verschubleiter/nuxt/config.e50178f0.js
+++
@@ -1,1 +0,0 @@
-import{cj as p,aV as o,l as i}from"./entry.de664525.js";const t={nuxt:{}},a=p(t);function s(){const n=i();return n._appConfig||(n._appConfig=o(a)),n._appConfig}export{s as u};
