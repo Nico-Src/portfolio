0fea6daf70345fec47b01b8774532026840bc5fd
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2025-02-10 21:58:36 +0100

    Pixelart Editor updated

--- projects/pixelart-editor/nuxt/config.8164c926.js
+++
@@ -1,1 +0,0 @@
-import{cC as p,aV as o,l as i}from"./entry.16d15c52.js";const t={nuxt:{}},a=p(t);function s(){const n=i();return n._appConfig||(n._appConfig=o(a)),n._appConfig}export{s as u};
---
+++ projects/pixelart-editor/nuxt/config.b91c6e72.js
@@ -0,0 +1,1 @@
+import{cC as p,aV as o,l as i}from"./entry.d993213a.js";const t={nuxt:{}},a=p(t);function s(){const n=i();return n._appConfig||(n._appConfig=o(a)),n._appConfig}export{s as u};
